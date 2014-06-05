e433c19a9e7c2fd5f40522a93af0eedd36fc1d9a
Dan Kleiman <info@dankleiman.com>
2014-06-05 13:26:34 -0400

    Added Ingredient class and ingredients to recipe

--- models/ingredient.rb
+++ models/ingredient.rb
@@ -1,2 +1,23 @@
 class Ingredient
+  attr_reader :id, :name, :recipe_id
+
+  def self.all
+    @ingredients = []
+
+    connection = PG::Connection.open(dbname: 'recipes')
+      ingredient_data = connection.exec( "SELECT * FROM ingredients" )
+    connection.close
+
+    ingredient_data.each do |ingredient|
+      @ingredients << Ingredient.new(ingredient["id"], ingredient["name"], ingredient["recipe_id"])
+    end
+
+    @ingredients
+  end
+
+  def initialize(id, name, recipe_id)
+    @id = id
+    @name = name
+    @recipe_id = recipe_id
+  end
 end
--- models/recipe.rb
+++ models/recipe.rb
@@ -1,22 +1,43 @@
 class Recipe
-  attr_reader :id, :name
+  attr_reader :id, :name, :description, :instructions
+
   def self.all
-    recipes = []
+    @recipes = []
 
     connection = PG::Connection.open(dbname: 'recipes')
       recipe_data = connection.exec( "SELECT * FROM recipes" )
     connection.close
 
-    recipe_data = recipe_data.to_a
     recipe_data.each do |recipe|
-      recipes << Recipe.new(recipe["id"], recipe["name"])
+      @recipes << Recipe.new(recipe["id"], recipe["name"], recipe["description"], recipe["instructions"])
     end
 
-    recipes
+    @recipes
   end
 
-  def initialize(id, name)
+  def initialize(id, name, description, instructions)
     @id = id
     @name = name
+    @description = description
+    @instructions = instructions
+  end
+
+  def self.find(id)
+    @recipes.each do |recipe|
+      if recipe.id == id
+        return recipe
+      end
+    end
+  end
+
+  def ingredients
+    ingredients = []
+    all_ingredients = Ingredient.all
+    all_ingredients.each do |ingredient|
+      if recipe_id = id
+        ingredients << ingredient
+      end
+    end
+    ingredients
   end
 end
